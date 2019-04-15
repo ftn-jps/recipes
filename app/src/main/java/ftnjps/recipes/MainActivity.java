58bd01628bbc1c70e0aa8e31acd97c15d893cf0b
Ivan Jancic <Ivan.Jancic@synechron.com>
2019-04-15 17:13:00 +0200

    DetailView implemented.

--- app/src/main/java/ftnjps/recipes/MainActivity.java
+++ app/src/main/java/ftnjps/recipes/MainActivity.java
@@ -18,6 +18,10 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -37,6 +41,65 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        ListView mListView = (ListView) findViewById(R.id.listView);
+
+        Recipe r1 = new Recipe("drawable://" + R.drawable.beer_ribs, "Rebarca pecena u rerni sa crnim pivom",
+                "Pripremati sa srcem, paziti da ne pregori i da se ne popije pivo.",
+                "Srednje",
+                3,
+                50,
+                "Prelijemo rebarca marinadom\nUbacimo u rernu\nIzvadimo iz rerne",
+                new Date()
+                );
+        Recipe r2 = new Recipe("drawable://" + R.drawable.oven_ribs, "Svinjska rebarca u marinadi od piva",
+                "Pazljivo pripremati sa dusom, paziti da ne pregori nista i da se ne popije svo pivo.",
+                "Srednje",
+                4,
+                30,
+                "Kupimo 1kg rebarca i 1L crnog piva po izboru\nPrelijemo rebarca marinadom i pivom\nUbacimo u rernu\nIzvadimo iz rerne",
+                new Date()
+        );
+        Recipe r3 = new Recipe("drawable://" + R.drawable.peas_ribs, "BBQ rebarca sa zapecenim graskom",
+                "Dobro spremiti BBQ od njega ce zavisiti krajnji rezultat.",
+                "Tesko",
+                10,
+                70,
+                "Napravimo BBQ\nGrilamo rebarca\nJedemo rebarca",
+                new Date()
+        );
+        Recipe r4 = new Recipe("drawable://" + R.drawable.roasted_pork_knukle, "Slasna kolenica",
+                "Izabrati dobar komad mesa u mesari.",
+                "Lako",
+                2,
+                65,
+                "Pazljivo poslozimo kolenicu u rernu\nPremazemo marinadom\nIzvadimo iz rerne",
+                new Date()
+        );
+        Recipe r5 = new Recipe("drawable://" + R.drawable.sauce_pork_knuckle, "Kolenica u sacu",
+                "Biti uz sac i mesati bez prestanka za bolji ukus.",
+                "Srednje",
+                5,
+                95,
+                "Spremimo materijal za sac\nNapravimo sac\nUbacimo kolenicu\nIzvadimo posle krckanja",
+                new Date()
+        );
+
+        ArrayList<Recipe> recipesList = new ArrayList<>();
+        recipesList.add(r1);
+        recipesList.add(r2);
+        recipesList.add(r3);
+        recipesList.add(r4);
+        recipesList.add(r5);
+        recipesList.add(r1);
+        recipesList.add(r2);
+        recipesList.add(r3);
+        recipesList.add(r4);
+        recipesList.add(r5);
+
+        RecipesListAdapter adapter = new RecipesListAdapter(this, R.layout.adapter_view_layout, recipesList);
+        mListView.setAdapter(adapter);
+
     }
 
     @Override
---
+++ app/src/main/java/ftnjps/recipes/RecipesListAdapter.java
@@ -0,0 +1,167 @@
+package ftnjps.recipes;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.nostra13.universalimageloader.cache.memory.impl.WeakMemoryCache;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.ImageScaleType;
+import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer;
+
+import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
+
+public class RecipesListAdapter extends ArrayAdapter<Recipe> {
+
+    private static final String TAG = "RecipeListAdapter";
+
+    private Context mContext;
+    private int mResource;
+    private int lastPosition = -1;
+
+    /**
+     * Holds variables in a View
+     */
+    private static class ViewHolder {
+        TextView recipeDescription;
+        ImageView recipeImage;
+    }
+
+    /**
+     * Default constructor for the PersonListAdapter
+     * @param context
+     * @param resource
+     * @param objects
+     */
+    public RecipesListAdapter(Context context, int resource, ArrayList<Recipe> objects) {
+        super(context, resource, objects);
+        mContext = context;
+        mResource = resource;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        //sets up the image loader library
+        setupImageLoader();
+
+        //get the recipe information
+        String recipeDescription = getItem(position).getRecipeDescription();
+        String imgUrl = getItem(position).getImgURL();
+
+        //create the view result for showing the animation
+        final View result;
+
+        //need final position for the inner class
+        final int finalPosition = position;
+
+        //ViewHolder object
+        ViewHolder holder;
+
+
+        if(convertView == null){
+            LayoutInflater inflater = LayoutInflater.from(mContext);
+            convertView = inflater.inflate(mResource, parent, false);
+            holder= new ViewHolder();
+            holder.recipeDescription = (TextView) convertView.findViewById(R.id.textView1);
+            holder.recipeImage = (ImageView) convertView.findViewById(R.id.image);
+
+            result = convertView;
+
+            convertView.setTag(holder);
+        }
+        else{
+            holder = (ViewHolder) convertView.getTag();
+            result = convertView;
+        }
+
+        lastPosition = position;
+
+        //create the imageloader object
+        ImageLoader imageLoader = ImageLoader.getInstance();
+
+        int defaultImage = mContext.getResources().getIdentifier("@drawable/image_failed",null,mContext.getPackageName());
+
+        //create display options
+        DisplayImageOptions options = new DisplayImageOptions.Builder().cacheInMemory(true)
+                .cacheOnDisc(true).resetViewBeforeLoading(true)
+                .showImageForEmptyUri(defaultImage)
+                .showImageOnFail(defaultImage)
+                .showImageOnLoading(defaultImage).build();
+
+        //download and display image from url
+        imageLoader.displayImage(imgUrl, holder.recipeImage, options);
+
+        holder.recipeDescription.setText(recipeDescription);
+        holder.recipeDescription.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, RecipeActivity.class);
+                intent.putExtra("RECIPE_ID", getItem(finalPosition).getId().toString());
+                intent.putExtra("RECIPE_TITLE", getItem(finalPosition).getTitle());
+                intent.putExtra("RECIPE_DESCRIPTION", getItem(finalPosition).getDescription());
+                intent.putExtra("RECIPE_DIFFICULTY", getItem(finalPosition).getDifficulty());
+                intent.putExtra("RECIPE_PREPARATION_STEPS", getItem(finalPosition).getPreparationSteps());
+                intent.putExtra("RECIPE_NUMBER_OF_PEOPLE", Integer.toString(getItem(finalPosition).getNumberOfPeople()));
+                intent.putExtra("RECIPE_TIME_OF_PREPARATION", Integer.toString(getItem(finalPosition).getTimeOfPreparation()));
+                intent.putExtra("RECIPE_CREATION_DATE", getItem(finalPosition).getCreationDate().toString());
+                intent.putExtra("RECIPE_IMAGE_URL", getItem(finalPosition).getImgURL());
+
+                mContext.startActivity(intent);
+                Toast.makeText(mContext, "Opening recipe...", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        holder.recipeImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, RecipeActivity.class);
+                intent.putExtra("RECIPE_ID", getItem(finalPosition).getId().toString());
+                intent.putExtra("RECIPE_TITLE", getItem(finalPosition).getTitle());
+                intent.putExtra("RECIPE_DESCRIPTION", getItem(finalPosition).getDescription());
+                intent.putExtra("RECIPE_DIFFICULTY", getItem(finalPosition).getDifficulty());
+                intent.putExtra("RECIPE_PREPARATION_STEPS", getItem(finalPosition).getPreparationSteps());
+                intent.putExtra("RECIPE_NUMBER_OF_PEOPLE", Integer.toString(getItem(finalPosition).getNumberOfPeople()));
+                intent.putExtra("RECIPE_TIME_OF_PREPARATION", Integer.toString(getItem(finalPosition).getTimeOfPreparation()));
+                intent.putExtra("RECIPE_CREATION_DATE", getItem(finalPosition).getCreationDate().toString());
+                intent.putExtra("RECIPE_IMAGE_URL", getItem(finalPosition).getImgURL());
+
+                mContext.startActivity(intent);
+                Toast.makeText(mContext, "Opening recipe...", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return convertView;
+    }
+
+    /**
+     * Required for setting up the Universal Image loader Library
+     */
+    private void setupImageLoader(){
+        // UNIVERSAL IMAGE LOADER SETUP
+        DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder()
+                .cacheOnDisc(true).cacheInMemory(true)
+                .imageScaleType(ImageScaleType.EXACTLY)
+                .displayer(new FadeInBitmapDisplayer(300)).build();
+
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(
+                mContext)
+                .defaultDisplayImageOptions(defaultOptions)
+                .memoryCache(new WeakMemoryCache())
+                .discCacheSize(100 * 1024 * 1024).build();
+
+        ImageLoader.getInstance().init(config);
+        // END - UNIVERSAL IMAGE LOADER SETUP
+    }
+}
