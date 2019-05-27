3b43d1e84732850084c20fc3921ae1bbc867d3c1
BojanStipic <bojanstipic@gmail.com>
2019-05-27 21:51:15 +0200

    Increase database schema version number

--- app/src/main/java/ftnjps/recipes/data/AppDatabase.java
+++ app/src/main/java/ftnjps/recipes/data/AppDatabase.java
@@ -4,7 +4,7 @@ import androidx.room.Database;
 import androidx.room.RoomDatabase;
 import androidx.room.TypeConverters;
 
-@Database(entities = {Recipe.class, Comment.class}, version = 1)
+@Database(entities = {Recipe.class, Comment.class}, version = 2)
 @TypeConverters({Converters.class})
 public abstract class AppDatabase extends RoomDatabase {
     public abstract RecipeDao recipeDao();
