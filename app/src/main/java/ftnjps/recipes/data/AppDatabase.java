32128e9f4c255efec2244a59cd966471cb06f915
Filip Petrovic <fpetrovic@ymail.com>
2019-07-01 19:35:03 +0200

    Implemented type converters for ingredients

--- app/src/main/java/ftnjps/recipes/data/AppDatabase.java
+++ app/src/main/java/ftnjps/recipes/data/AppDatabase.java
@@ -4,8 +4,10 @@ import androidx.room.Database;
 import androidx.room.RoomDatabase;
 import androidx.room.TypeConverters;
 
+import ftnjps.recipes.data.typeconverters.MapTypeConverter;
+
 @Database(entities = {Recipe.class, Comment.class}, version = 3)
-@TypeConverters({Converters.class})
+@TypeConverters({Converters.class, MapTypeConverter.class})
 public abstract class AppDatabase extends RoomDatabase {
     public abstract RecipeDao recipeDao();
     public abstract CommentDao commentDao();
